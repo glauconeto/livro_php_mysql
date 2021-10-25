593cbafe4c0d2cdd360ca36def5ccb35d533ff64
glauconeto <neto.glauquinho@gmail.com>
2021-10-25 15:02:33 -0300

    removi pasta do PHPMailer

--- tarefas_oo_seguranca/bibliotecas/PHPMailer/inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require __DIR__ . 'src/Exception.php';
-require __DIR__ . 'src/OAuth.php';
-require __DIR__ . 'src/PHPMailer.php';
-require __DIR__ . 'src/POP3.php';
-require __DIR__ . 'src/SMTP.php';
